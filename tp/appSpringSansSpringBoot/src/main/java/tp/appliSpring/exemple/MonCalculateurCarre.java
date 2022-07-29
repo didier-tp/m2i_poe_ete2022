5c45dedb20ee8cb8f0bc313bfabbc461fae271f7
DidierDefrance <didier@d-defrance.fr>
2022-07-29 11:59:46 +0200

    debut appliSpring (springBoot)

--- tp/appSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonCalculateurCarre.java
+++ tp/appSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonCalculateurCarre.java
@@ -1,8 +1,10 @@
 package tp.appliSpring.exemple;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Lazy
 public class MonCalculateurCarre implements MonCalculateur {
 
 	public double calculer(double x) {
