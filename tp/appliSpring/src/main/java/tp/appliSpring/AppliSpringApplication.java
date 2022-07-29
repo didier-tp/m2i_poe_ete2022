fe989d749efd6e1e200fa4a7d16ef007bc244c47
DidierDefrance <didier@d-defrance.fr>
2022-07-29 12:33:08 +0200

    s

--- tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -3,12 +3,18 @@ package tp.appliSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//NB: @SpringBootApplication est un équivalent
+//de @Configuration + @EnableAutoConfiguration + @ComponentScan/current package
+//    + ...
+
 @SpringBootApplication
 public class AppliSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppliSpringApplication.class, args);
-		//é , à
+		System.out.println("http://localhost:8080/appliSpring");
+		//http://localhost:8080/appliSpring/index.html
+		//et index.html sera trouvé dans src/main/resources/static
 	}
 
 }
