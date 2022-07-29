61671386a35c8928e0d2631def295c217e1ab8db
DidierDefrance <didier@d-defrance.fr>
2022-07-29 15:01:54 +0200

    s

--- tp/appliSpring/src/test/java/tp/appliSpring/core/dao/TestCompteDao.java
+++ tp/appliSpring/src/test/java/tp/appliSpring/core/dao/TestCompteDao.java
@@ -1,13 +1,17 @@
 package tp.appliSpring.core.dao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import tp.appliSpring.AppliSpringApplication;
+import tp.appliSpring.core.entity.Compte;
 
 
 //@RunWith(SpringRunner.class)  //si junit4
@@ -15,9 +19,24 @@ import tp.appliSpring.AppliSpringApplication;
 @SpringBootTest(classes= {AppliSpringApplication.class})
 public class TestCompteDao {
 	
-private static Logger logger = LoggerFactory.getLogger(TestCompteDao.class);
+    private static Logger logger = LoggerFactory.getLogger(TestCompteDao.class);
 	
 	@Autowired
+	//@Qualifier("simu")
+	@Qualifier("jpa")
 	private DaoCompte daoCompte; //à tester
+	
+	@Test
+	public void testAjoutEtRelecture() {
+		//hypothese : base et table vides au lancement du test
+		Compte compte = new Compte(null,"compteA",100.0);
+		Compte compteSauvegarde = this.daoCompte.save(compte); //INSERT INTO
+		logger.debug("compteSauvegarde=" + compteSauvegarde);
+		
+		Compte compteRelu = this.daoCompte.findById(compteSauvegarde.getNumero()); //SELECT
+		Assertions.assertEquals("compteA",compteRelu.getLabel());
+		Assertions.assertEquals(100.0,compteRelu.getSolde());
+		logger.debug("compteRelu=" + compteRelu);
+	}
 
 }
