5cb9e4070cdd6fc3e69082c92457bf4cf8cc70a1
DidierDefrance <didier@d-defrance.fr>
2022-08-02 10:48:11 +0200

    s

--- tp/appliSpring/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import tp.appliSpring.core.dao.DaoClient;
 import tp.appliSpring.core.dao.DaoCompte;
+import tp.appliSpring.core.entity.Client;
 import tp.appliSpring.core.entity.Compte;
 
 @Service //@Service = @Component de type service métier
@@ -12,6 +14,15 @@ public class ServiceCompteImpl implements ServiceCompte {
 	//à compléter
 	
 	private DaoCompte daoCompte; //dépendance à injecter
+	
+	private DaoClient daoClient; //dépendance à injecter
+	
+	/* @Autowired */
+	public ServiceCompteImpl(DaoCompte daoCompte,DaoClient daoClient){
+		//injection possible via constructeur (comme angular)
+		this.daoCompte = daoCompte;
+		this.daoClient = daoClient;
+	}
 
 	@Override
 	public Compte rechercherCompteParNumero(long numCompte) {
@@ -23,33 +34,39 @@ public class ServiceCompteImpl implements ServiceCompte {
 		// on remonte en mémoire tout l'objet compteDeb selon son numero (via daoCompte)
 		// on débite de l'argent sur ce compte
 		// on sauvegarde en base le compte modifié
+		Compte cptDeb = this.daoCompte.findById(numCptDeb).get();
+		cptDeb.setSolde(cptDeb.getSolde() - montant);
+		this.daoCompte.save(cptDeb);
 		
 		//idem pour compte à créditer
-		
+		Compte cptCred= this.daoCompte.findById(numCptCred).get();
+		cptCred.setSolde(cptCred.getSolde() + montant);
+		this.daoCompte.save(cptCred);
 	}
 
 	@Override
 	public List<Compte> rechercherComptesPourClient(long numClient) {
-		// TODO Auto-generated method stub
-		return null;
+		Client clientAvecSesComptes = this.daoClient.findClientByIdWithComptes(numClient);
+		return clientAvecSesComptes.getComptes();
 	}
 
 	@Override
 	public void supprimerCompte(long numCompte) {
-		// TODO Auto-generated method stub
-		
+		 daoCompte.deleteById(numCompte);
 	}
 
 	@Override
 	public Compte sauvegarderNouveauCompte(Compte compte) {
-		// TODO Auto-generated method stub
-		return null;
+		daoCompte.save(compte);
+		return compte;
 	}
 
 	@Override
 	public void mettreAJourCompte(Compte compte) {
-		// TODO Auto-generated method stub
-		
+		if(daoCompte.existsById(compte.getNumero()))
+		    daoCompte.save(compte);
+		else 
+			throw new RuntimeException("cannot update not existing compte :" + compte.toString());
 	}
 
 	
