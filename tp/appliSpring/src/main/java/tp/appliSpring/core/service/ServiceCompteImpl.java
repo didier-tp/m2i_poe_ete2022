edf893c7bccc34b54a2d1a49eefbdac3e6a3002a
DidierDefrance <didier@d-defrance.fr>
2022-08-02 14:51:47 +0200

    @Transactional

--- tp/appliSpring/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/core/service/ServiceCompteImpl.java
@@ -3,6 +3,8 @@ package tp.appliSpring.core.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import tp.appliSpring.core.dao.DaoClient;
 import tp.appliSpring.core.dao.DaoCompte;
@@ -10,6 +12,7 @@ import tp.appliSpring.core.entity.Client;
 import tp.appliSpring.core.entity.Compte;
 
 @Service //@Service = @Component de type service métier
+//@Transactional //en placant @Transactional sur le haut de la classe = bonne pratique , on évite des oublis 
 public class ServiceCompteImpl implements ServiceCompte {
 	//à compléter
 	
@@ -26,22 +29,46 @@ public class ServiceCompteImpl implements ServiceCompte {
 
 	@Override
 	public Compte rechercherCompteParNumero(long numCompte) {
-		return daoCompte.findById(numCompte).get(); //ou bien .orElse(null) ou bien ..
+		return daoCompte.findById(numCompte).orElse(null); //ou bien .get() ou bien ..
+	}
+	
+	
+	//sans @Transactional
+	public void effectuerVirementSansTransaction(double montant, long numCptDeb, long numCptCred) {
+	
+		//sans transaction globale au niveau du service , daoCompte.findById() 
+		//execute une minuscule petite transaction commité/fermée
+		//et cptDeb est à l'état détaché
+		Compte cptDeb = this.daoCompte.findById(numCptDeb).get();
+		cptDeb.setSolde(cptDeb.getSolde() - montant);
+		this.daoCompte.save(cptDeb);//on appelle explicitement .save() pour sauvegarder en base les modifications
+		                            //effectuées sur l'objet à l'état détache de JPA/Hibernate
+		//sans transaction globale au niveau du service , daoCompte.save() modifie définitivement l'état de la base
+		//ça ne pourra plus être annulé
+		
+		Compte cptCred= this.daoCompte.findById(numCptCred).get();
+		cptCred.setSolde(cptCred.getSolde() + montant);
+		this.daoCompte.save(cptCred);
 	}
 	
 	@Override
+	@Transactional(/*propagation = Propagation.REQUIRED*/) //REQUIRED par defaut
 	public void effectuerVirement(double montant, long numCptDeb, long numCptCred) {
-		// on remonte en mémoire tout l'objet compteDeb selon son numero (via daoCompte)
-		// on débite de l'argent sur ce compte
-		// on sauvegarde en base le compte modifié
-		Compte cptDeb = this.daoCompte.findById(numCptDeb).get();
+		// transaction globale initialisée dès le début de l'exécution de effectuerVirement
+		Compte cptDeb = this.daoCompte.findById(numCptDeb).get(); //le dao exécute son code dans la grande transaction
+		//commencée par le service sans la fermer et l'objet cptDeb remonte à l'état persistant
+		
 		cptDeb.setSolde(cptDeb.getSolde() - montant);
-		this.daoCompte.save(cptDeb);
+		//this.daoCompte.save(cptDeb); //appel de .save() possible et dans ce cas base modifiée temporairement seulement
+		                               //avec rollback ultérieur possible en cas d'exception
 		
 		//idem pour compte à créditer
 		Compte cptCred= this.daoCompte.findById(numCptCred).get();
 		cptCred.setSolde(cptCred.getSolde() + montant);
-		this.daoCompte.save(cptCred);
+		//this.daoCompte.save(cptCred);
+		
+		//en fin de transaction réussie (sans exception) , toutes les modification effectuées sur les objets
+		//à l'état persistant seront répercutées en base (.save() automatiques)
 	}
 
 	@Override
