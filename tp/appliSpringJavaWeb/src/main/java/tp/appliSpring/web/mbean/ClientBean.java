42d76f9f5ec0bc69fd384e6c0293cfdd6bab45a8
DidierDefrance <didier@d-defrance.fr>
2022-08-03 16:50:10 +0200

    avec jsf intégré dans spring

---
+++ tp/appliSpringJavaWeb/src/main/java/tp/appliSpring/web/mbean/ClientBean.java
@@ -0,0 +1,47 @@
+package tp.appliSpring.web.mbean;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tp.appliSpring.core.entity.Client;
+import tp.appliSpring.core.service.ServiceClient;
+
+@ManagedBean
+//@RequestScoped
+@SessionScoped
+public class ClientBean {
+	
+	@Autowired
+	private ServiceClient serviceClient;
+	
+	private Long numClient ;
+	private Client client;
+	
+	public String doLogin() {
+		this.client = serviceClient.rechercherClientAvecComptesByNum(numClient);
+		return "clientComptes" ; // clientComptes.xhtml
+	}
+	
+
+	public ClientBean() {
+		super();
+	}
+	
+	public Long getNumClient() {
+		return numClient;
+	}
+	public void setNumClient(Long numClient) {
+		this.numClient = numClient;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	
+
+}
