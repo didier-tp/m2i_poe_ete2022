e4d2a106a18a63112371725b5701e48751648b0b
DidierDefrance <didier@d-defrance.fr>
2022-08-03 16:07:03 +0200

    @SessionAttributes

--- tp/appliSpringJavaWeb/src/main/java/tp/appliSpring/web/controller/ClientController.java
+++ tp/appliSpringJavaWeb/src/main/java/tp/appliSpring/web/controller/ClientController.java
@@ -3,13 +3,19 @@ package tp.appliSpring.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import tp.appliSpring.core.entity.Client;
 import tp.appliSpring.core.service.ServiceClient;
 
 @Controller  //cas particulier de @Component (pour crontroller web de spring mvc)
+@SessionAttributes( value={"client"} )
+//noms des "modelAttributes" qui sont EN PLUS récupérés/stockés
+//en SESSION HTTP au niveau de la page de rendu
+//--> visibles en requestScope ET en sessionScope
 public class ClientController {
 	
 	@Autowired
@@ -25,6 +31,10 @@ public class ClientController {
 		return "virement"; //pour demander la vue jsp/virement.jsp
 	}
 	
+	@ModelAttribute("client")
+	public Client addClientInModel() {
+		return new Client(); //valeur par defaut
+	}
 	
 	@RequestMapping("/verifLogin")
 	public String verifLogin(Model model,@RequestParam(name="numClient",required =false ) Long numClient) {
@@ -34,7 +44,7 @@ public class ClientController {
 		}
 		
 		Client clientAvecSesComptes = serviceClient.rechercherClientAvecComptesByNum(numClient);
-		model.addAttribute("client", clientAvecSesComptes);
+		model.addAttribute("client", clientAvecSesComptes); //V1 et V2
 		return "clientComptes"; //pour demander la vue jsp/clientComptes.jsp
 	}
 
