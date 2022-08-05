e515effc3f50b0bcc927a9fd5317bb514237d605
DidierDefrance <didier@d-defrance.fr>
2022-08-05 16:37:02 +0200

    s

--- tp/appliSpring/lancerAppliSpringBoot.bat
+++ tp/appliSpring/lancerAppliSpringBoot.bat
@@ -2,5 +2,5 @@ cd /d "%~dp0/target"
 REM NB: in META-INF/MANIFEST.MF of .jar
 REM Main-Class: org.springframework.boot.loader.PropertiesLauncher
 REM Start-Class: tp.appliSpring.AppliSpringApplication
-java -Dspring.profiles.active=reInit,embeddedDb -jar appliSpring.jar
+java -Dspring.profiles.active=reInit,embeddedDb -jar appliSpring-0.0.1-SNAPSHOT.jar
 pause
